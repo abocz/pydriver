f260c7a278bf771000835f68cc02a746f18db26b
Derek McNeil <derek.mcneil90@gmail.com>
2016-04-20 22:51:42 -0400

    Fix github url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pydriver',
       version='0.1',
       description='',
-      url='http://github.com/dmcneil/py-driver',
+      url='http://github.com/dmcneil/pydriver',
       author='Derek McNeil',
       author_email='derek.mcneil90@gmail.com',
       license='MIT',
